b4aadf875bbed9c3d83abb6d6f8fa94c9b027a58
Dan Anderson <danfanderson@gmail.com>
2020-06-23 17:06:57 -0500

    Clean up codes and comments

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # Standard imports
 import os
-import pickle
-import sklearn
-import pandas as pd
-import numpy as np
 import re
+import pickle
+import pandas as pd
+import numpy  as np
 
 # Modeling/vectorizing imports
+import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 import spacy
 
@@ -27,7 +27,7 @@ loc_nn    = os.environ.get("URL_MODEL_NN", default="nn_model.pkl")
 # Instantiate a spacy object
 nlp = spacy.load("en_core_web_sm")
 
-# Regular expression used to remove non-standard characters
+# Regular expressions used to remove non-standard characters
 rgxNotStdChars = re.compile(r'[^a-zA-z0-9.,!?/:;\"\'\s]')
 rgxMultWhtSpce = re.compile(r'\s{2,}')
 
@@ -79,12 +79,12 @@ def dummy_func(doc):
 
 # Read the Nearest Neighbors model
 print(f'INFO: loading the recommendation model')
-# Load the model from the web
+# Load the model from the web if deployed to Heroku
 if is_heroku:
   print(f'INFO: not using Heroku as this time. Terminating.')
   quit()
 
-# Load the model from disk
+# Load the model from disk if deployed other than Heroku
 if not is_heroku:
   pkl_file = open(loc_nn, 'rb')
   nn       = pickle.load(pkl_file)
@@ -105,62 +105,74 @@ print(f'INFO: loaded {len(df.index)} dataframe rows')
 print(f'INFO: generating a map of dataframe rows/elements')
 dict_df = df.to_dict('records')
 
-# /status can be used to verify the Flask app is running and responding
+# /status can be used to verify that the Flask app is running and responding
 @app.route('/status')
 def test():
-    num_strains = len(dict_df)
-    ret_dict = {}
-    ret_dict["msg"] = "app.py is up and running"
-    ret_dict["note"] = "the number of strains in the dataset is: " + str(num_strains)
+  num_strains = len(dict_df)
+  ret_dict = {}
+  ret_dict["msg"] = "app.py is up and running"
+  ret_dict["note"] = "the number of strains in the dataset is: " + str(num_strains)
 
-    return jsonify(ret_dict)
+  return jsonify(ret_dict)
 
 # /get_recommendation accepts a string of text and returns a collection of strain recommendations
 #     and associated metadata
 @app.route('/get_recommendation', methods = ['POST'])
 def get_recommendation():
-    # Get the request's body as json
-    jsn = request.get_json(force=True)
+  # Get the request's body as json
+  jsn      = request.get_json(force=True)
+  ret_dict = {}
 
-    # Process the inbound text and generate a list of tokens
-    tmp_text = retain_std_chars(jsn["text"])
-    tmp_list = tnkize_text(tmp_text) 
+  # Validate the inbound json body parameters
+  tmp_string = jsn["text"]
+  if len(tmp_string) == 0:
+    # missing text value
+    ret_dict["msg"] = "missing text value"
+    return jsonify(ret_dict), 400
 
-    # Score the list of tokends
-    tmp_score = tfidf.transform([tmp_list])
-    tmp_rslt  = nn.kneighbors(tmp_score.todense())
+  # Process the inbound text and generate a list of tokens
+  tmp_text = retain_std_chars(jsn["text"])
+  tmp_list = tnkize_text(tmp_text) 
 
-    # Determine the row number of the first result
-    rel_ranks = tmp_rslt[0][0]
-    rel_docs  = tmp_rslt[1][0]
+  # Score the list of tokends
+  tmp_score = tfidf.transform([tmp_list])
+  tmp_rslt  = nn.kneighbors(tmp_score.todense())
 
-    # Iterate through the document references
-    ret_list = []
-    for idx, elm in np.ndenumerate(rel_docs):
-      tmp_dict = dict_df[rel_docs[idx[0]]]
+  # Grab the reccomendation scores/rankings and references
+  rel_ranks = tmp_rslt[0][0]
+  rel_docs  = tmp_rslt[1][0]
 
-      # Construct a temporary map
-      ret_dict = {}
-      ret_dict['description']       = tmp_dict['Description']
-      ret_dict['effects']           = tmp_dict['Effects']
-      ret_dict['flavor']            = tmp_dict['Flavor']
-      ret_dict['symptoms_diseases'] = tmp_dict['symptoms_diseases']
-      ret_dict['rating']            = tmp_dict['Rating']
-      ret_dict['strain']            = tmp_dict['Strain']
-      ret_dict['type']              = tmp_dict['Type']
-      ret_dict['df_index']          = int(rel_docs[idx[0]])
-      ret_dict['score']             = round(rel_ranks[idx[0]], 3)
-      
-      ret_list.append(ret_dict)
+  # Iterate through the document references
+  ret_list = []
+  for idx, elm in np.ndenumerate(rel_docs):
+    tmp_dict = dict_df[rel_docs[idx[0]]]
 
-    # Construct the return object
-    return_object = {}
-    return_object["msg"]              = "your recommendations"
-    return_object["text"]             = jsn["text"]
-    return_object["recommendations"]  = ret_list
+    # Construct a temporary map
+    ret_dict = {}
+    ret_dict['description']       = tmp_dict['Description']
+    ret_dict['effects']           = tmp_dict['Effects']
+    ret_dict['flavor']            = tmp_dict['Flavor']
+    ret_dict['symptoms_diseases'] = tmp_dict['symptoms_diseases']
+    ret_dict['rating']            = tmp_dict['Rating']
+    ret_dict['strain']            = tmp_dict['Strain']
+    ret_dict['type']              = tmp_dict['Type']
+    ret_dict['df_index']          = int(rel_docs[idx[0]])
+    ret_dict['score']             = round(rel_ranks[idx[0]], 3)
+    
+    ret_list.append(ret_dict)
 
-    return jsonify(return_object)
+  # Construct the return object
+  return_object = {}
+  return_object["msg"]              = "your recommendations"
+  return_object["text"]             = jsn["text"]
+  return_object["recommendations"]  = ret_list
+  return_object["length"]           = len(ret_list)
+
+  return jsonify(return_object)
 
 # Start the flask listening on localhost:5000
 if __name__ == '__main__':
+  app_domain = 'localhost'
+  app_port   = 5000
+  print(f'INFO: starting web api server on {app_domain}:{app_port}')
   app.run(host='localhost', port=5000)
